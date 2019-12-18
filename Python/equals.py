540ed6b2411dcbfe7e55424d20447b26bdd25d9d
Wesley Ferreira <wsf.ley@gmail.com>
2019-12-18 19:41:20 -0300

    trabalhos do wildcards.

--- Python/equals.py
+++ Python/equals.py
@@ -1,21 +1,31 @@
 import ast
 def isEquals(node1, node2):
-    # verificacao da cabeca da arvore. Se diferente entao.
-    # print(node1, node2)
     if type(node1) != type(node2):
         return False
     if(isinstance(node1, ast.AST)):
-        #print('>isinstance(node1, ast.AST) => TRUE')
         for tipe, var in vars(node1).items():
-            #print('->>>>>', 'tipe: ', tipe, '->>>>> var: ' ,var)
             if tipe not in ('lineno', 'col_offset', 'ctx'):
+                # WILDCARDS
+                ctx_var = vars(node1).get('ctx')
+                # ignorar nomes em variaveis.
+                # Se o Node1 for uma atribuição (AST.STORE), ignore.
+                if isinstance(ctx_var, ast.Store):
+                    return True
+
+                # Ignorar argumentos de chamadas de funcoes.
+                if isinstance(node1, ast.Call) and tipe == 'args':
+                    list_node1 = vars(node1).get(tipe)
+                    list_node2 = vars(node2).get(tipe)
+                    if len(list_node1) == len(list_node2):
+                        return True
                 var2 = vars(node2).get(tipe)
                 if not isEquals(var, var2):
-                    return False        
+                    return False
         return True
+        #
+        #  Verificação de list dentro dos nós (args)
+        #
     elif isinstance(node1, list):
-        #print('>isinstance(node1, ast.AST) => FALSE')
-        #print('',node1, list)
         if len(node1) != len(node2):
             return False
         for i in range(len(node1)):
--- Python/subtree.py
+++ Python/subtree.py
@@ -1,8 +1,10 @@
 import ast
 import equals
+
 class Analyzer(ast.NodeVisitor):
     __error = []
     errors = []
+
     def status(self):
         list_subtree = list(ast.iter_child_nodes(self.sub_tree))
         status_value = self.__verify_error(self.__error, list_subtree)
@@ -11,6 +13,7 @@ class Analyzer(ast.NodeVisitor):
             return True
         else:
             return False
+
     def __verify_error(self, error, subtree):
         error_node = []
         error_node_subtree = []
@@ -21,9 +24,10 @@ class Analyzer(ast.NodeVisitor):
         if len(subtree) == 1:
             return len(error)
         else:
-            cont = sum(1 for indexI in range(len(error_node_subtree)) if error_node_subtree[indexI:indexI+len(subtree)] == subtree)
-            #print(cont)
+            cont = sum(1 for indexI in range(len(error_node_subtree))
+                       if error_node_subtree[indexI:indexI+len(subtree)] == subtree)
             return cont
+
     def detals_erro(self, errors=None, error=None):
         if errors == None and error == None:
             return None
@@ -33,22 +37,18 @@ class Analyzer(ast.NodeVisitor):
             return error_dict
         else:
             return [vars(indexI) for indexI in errors]
-        
 
-        
-    def length_subtree(self, subtree):        
+    def length_subtree(self, subtree):
         return len(vars(subtree).get('body'))
-        
-        
-    def __init__(self, tree, sub_tree):  
+
+    def __init__(self, tree, sub_tree):
         self.tree = tree
-        self.sub_tree = sub_tree        
+        self.sub_tree = sub_tree
         ast.NodeVisitor.generic_visit(self, tree)
 
-    
     def generic_visit(self, node):
         for node_subtree in ast.iter_child_nodes(self.sub_tree):
-            value = equals.isEquals(node,node_subtree)
+            value = equals.isEquals(node, node_subtree)
             if(value):
                 lis_aux = []
                 lis_aux.append(node)
