8682c50710165260d1a0670a350f135ec5317ebd
= <=>
2020-01-16 21:43:52 -0300

    trabalhando diferença entre variaveis e funcoes.

--- Python/equals.py
+++ Python/equals.py
@@ -1,5 +1,6 @@
 import ast
 def isEquals(node1, node2):
+    print(node1)
     if type(node1) != type(node2):
         return False
     if(isinstance(node1, ast.AST)):
@@ -10,7 +11,8 @@ def isEquals(node1, node2):
                 # ignorar nomes em variaveis.
                 # Se o Node1 for uma atribuição (AST.STORE), ignore.
                 if isinstance(ctx_var, ast.Store):
-                    return True
+                    return True 
+        
 
                 # Ignorar argumentos de chamadas de funcoes.
                 if isinstance(node1, ast.Call) and tipe == 'args':
@@ -25,10 +27,21 @@ def isEquals(node1, node2):
         #
         #  Verificação de list dentro dos nós (args)
         #
-    elif isinstance(node1, list):
-        if len(node1) != len(node2):
+    elif isinstance(node1, list):                
+        if len(node1) != len(node2):            
             return False
         for i in range(len(node1)):
+            aux_node = node1[i];
+            value_aux_node = vars(aux_node).get('value')
+            # print(value_aux_node)
+            if(value_aux_node != None):
+                # 
+                for walkin in ast.walk(value_aux_node):
+                    if(isinstance(walkin, ast.Name)):
+                        print(node1, vars(walkin))
+                        ctx_node1 = vars(walkin).get("ctx")
+                        print(vars(ctx_node1))                    
+            
             if not isEquals(node1[i], node2[i]):
                 return False
         return True
