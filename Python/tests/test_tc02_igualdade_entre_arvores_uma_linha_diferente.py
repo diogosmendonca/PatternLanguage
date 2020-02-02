32b3eb4a164315902f16df08aac41d5827c8b8c9
Wesley Ferreira <wsf.ley@gmail.com>
2020-02-02 20:20:42 -0300

    Testes automatizados: igualdade entre arvores.

---
+++ Python/tests/test_tc02_igualdade_entre_arvores_uma_linha_diferente.py
@@ -0,0 +1,23 @@
+from src.SourceTree import SourceTree
+import ast
+
+
+def test_is_equals():
+
+    source1 = """
+print("Ola Mundo")
+    """
+
+    source2 = """
+a = 10
+    """
+
+    parse_source1 = ast.parse(source1)
+    parse_source2 = ast.parse(source2)
+
+    tree = SourceTree(parse_source1)
+
+    ERR_MESSAGE = "As AST deveriam ser diferentes."
+
+    assert False == (tree.is_equals(parse_source2)), ERR_MESSAGE
+
