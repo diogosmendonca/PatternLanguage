b1d9e476bc30501152cfc53362a7505ffa7149a2
Wesley Ferreira <wsf.ley@gmail.com>
2020-02-15 10:43:22 -0300

    Testes: comparacao entre arvores

--- Python/tests/test_aceitacao.py
+++ Python/tests/test_aceitacao.py
@@ -41,3 +41,25 @@ a = 10
 
     assert False == (tree.is_equals(pattern)), ERR_MESSAGE
 
+def test_tc03_comparacao_subarvore_uma_linha_igual():
+    str_source = """
+def exemplo ():
+    a = 10
+    print(10)
+    """
+
+    str_pattern = """
+a = 10
+    """
+    source = ast.parse(str_source)
+    pattern = ast.parse(str_pattern)
+
+    tree = SourceTree(source)
+
+    ERR_MESSAGE = "A AST do codigo fonte deveria conter o padrão"
+
+    positions = tree.get_positions_pattern(pattern)
+    print("Posicao do padrao: ", positions)
+
+    assert True == (tree.is_subtree(pattern)), ERR_MESSAGE
+
