6928db2deb6737629bbed1053347154a6e946aa8
Wesley Ferreira <wsf.ley@gmail.com>
2020-02-26 14:48:02 -0300

    Ajustando assert de Posicoes.

--- Python/tests/test_aceitacao.py
+++ Python/tests/test_aceitacao.py
@@ -13,15 +13,11 @@ print("Ola Mundo")
 
     source = ast.parse(str_source)
     pattern = ast.parse(str_pattern)
-
     tree = SourceTree(source)
-
     ERR_MESSAGE = "As AST deveriam ser iguais."
 
-    positions =  tree.get_positions_pattern(pattern)
-    print("Posicao do padrao: ", positions)
-
     assert True == (tree.is_equals(pattern)), ERR_MESSAGE
+
 
 def test_tc02_igualdade_entre_arvores_uma_linha_diferente():
     str_source = """
@@ -31,15 +27,13 @@ print("Ola Mundo")
     str_pattern = """
 a = 10
         """
-
     source = ast.parse(str_source)
     pattern = ast.parse(str_pattern)
-
     tree = SourceTree(source)
-
     ERR_MESSAGE = "As AST deveriam ser diferentes."
 
     assert False == (tree.is_equals(pattern)), ERR_MESSAGE
+
 
 def test_tc03_comparacao_subarvore_uma_linha_igual():
     str_source = """
@@ -56,12 +50,13 @@ a = 10
 
     tree = SourceTree(source)
 
-    ERR_MESSAGE = "A AST do codigo fonte deveria conter o padrão"
+    ERR_MESSAGE_SUBTREE = "A AST do codigo fonte deveria conter o padrão"
+    ERR_MESSAGE_POSITION = "As posições não conferem."
 
-    positions = tree.get_positions_pattern(pattern)
-    print("Posicao do padrao: ", positions)
+    positions = tree.get_simple_position_pattern(pattern)
+    assert positions == ([[({'lineno': 3, 'col_offset': 4}, {'lineno': 3, 'col_offset': 8})]]), ERR_MESSAGE_POSITION
+    assert True == (tree.is_subtree(pattern)), ERR_MESSAGE_SUBTREE
 
-    assert True == (tree.is_subtree(pattern)), ERR_MESSAGE
 
 def test_tc04_comparacao_subarvore_multiplas_linhas_existe():
     str_source = """
@@ -81,10 +76,8 @@ print(10)
 
     ERR_MESSAGE = "A AST do codigo fonte deveria conter o padrão"
 
-    positions = tree.get_positions_pattern(pattern)
-    print("Posicao do padrao: ", positions)
-
     assert True == (tree.is_subtree(pattern)), ERR_MESSAGE
+
 
 def test_tc05_comparacao_subarvore_multiplas_linhas_nao_existe():
     str_source = """
@@ -104,10 +97,8 @@ string = "String"
 
     ERR_MESSAGE = "A AST do codigo fonte nao deveria conter o padrão"
 
-    positions = tree.get_positions_pattern(pattern)
-    print("Posicao do padrao: ", positions)
-
     assert False == (tree.is_subtree(pattern)), ERR_MESSAGE
+
 
 def test_tc06_wildcards_any_variable_uma_variavel():
     str_source = """
@@ -127,8 +118,7 @@ anyVariable = 10
 
     ERR_MESSAGE = "A AST do codigo fonte deveria conter o padrão"
 
-    positions = tree.get_positions_pattern(pattern)
-
-    # assert [[{'lineno': 4, 'col_offset': 4}]] == positions
+    positions = tree.get_simple_position_pattern(pattern)
+    print(positions)
+    assert [[({'lineno': 4, 'col_offset': 4}, {'lineno': 4, 'col_offset': 8})]] == positions
     assert True == (len(tree.get_all_occurrences(pattern)) == 1), ERR_MESSAGE
-
