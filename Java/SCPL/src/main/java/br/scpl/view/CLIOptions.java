3f2ba3cafc8e35f9a09af5e70e6de663ee5d74db
unknown <deniscrispe@gmail.com>
2020-05-23 01:17:52 -0300

    Inclusão da opção verbose

--- Java/SCPL/src/main/java/br/scpl/view/CLIOptions.java
+++ Java/SCPL/src/main/java/br/scpl/view/CLIOptions.java
@@ -1,9 +1,17 @@
 package br.scpl.view;
 
 import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 
 public class CLIOptions extends JCommander {
 	
-	//Put CLI options here
+	@Parameter(names = {"-V", "--verbose"}, description = "Activate the mode that displays extended information")
+	 private static boolean verbose = false;
 
+	//Put CLI options here
+	
+	public static boolean isVerbose() {
+		return verbose;
+	}
+	
 }
