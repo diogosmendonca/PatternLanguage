0826868dd5e2345636a7b72e2e5aa11cf8ff2f76
Denis <deniscrispe@gmail.com>
2020-08-30 21:42:15 -0300

    Teste unitário

--- Java/SCPL/src/test/resources/AceitacaoFiles/TC94_Pattern.java
+++ Java/SCPL/src/test/resources/AceitacaoFiles/TC94_Pattern.java
@@ -1,5 +1,5 @@
 
-@AlertIfNotDefaultAccess
+@AlertIfNotDefaultAccess("ruleid=10, type=BUG, severity=MAJOR, message =a msg")
 class SourceCode{
     public static void run() {
         System.out.println("Hello Nico");
