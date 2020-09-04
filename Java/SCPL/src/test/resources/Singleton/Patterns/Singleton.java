82484867201e93a1dcb69a2add9a89fd0cd0b0cd
unknown <deniscrispe@gmail.com>
2020-09-03 22:52:56 -0300

    Acerto no teste unitário template do padrão Singleton

--- Java/SCPL/src/test/resources/Singleton/Patterns/Singleton.java
+++ Java/SCPL/src/test/resources/Singleton/Patterns/Singleton.java
@@ -1,38 +1,39 @@
-public class SomeSingleton {
+public class SomeSingleton { 
 
-    @AlertIfNotPrivate("Instance should be private") 
-    @AlertIfNotStatic("Instance should be static") 
-    private static SomeSingleton instance;
-   
-    @AlertIfNotPrivate("A singleton constructor should be private")       
+    @AlertIfNotPrivate("ruleid=101, type=BUG, severity=MAJOR, message=Instance should be private") 
+    @AlertIfNotStatic("ruleid=102, type=BUG, severity=MAJOR, message=Instance should be static") 
+    private static SomeSingleton instance; 
+
+    @AlertIfNotPrivate("ruleid=103, type=BUG, severity=MAJOR, message=A singleton constructor should be private")       
     private SomeSingleton() {}
 
-    @AlertIfNotPublic @AlertIfNotStatic
+    @AlertIfNotPublic("ruleid=104, type=BUG, severity=MAJOR, message=Method should be public")  
+    @AlertIfNotStatic("ruleid=105, type=BUG, severity=MAJOR, message=Method should be static")
     public static SomeSingleton getInstance() {
         //#BEGIN
 
-        //Precisa ter um caso com a implementação correta, por causa das violações de modificadores
+        //CORRECT CASE
         if(instance == null)
-			instance = new someSingleton();
-		return instance;
+            instance = new someSingleton();
+        return instance;
 
         //#OR
 
         //not_exists
         if(instance == null)
-            //Exists Alert: Instance must be verified for null
+            //Alert(ruleid=106, type=BUG, severity=MAJOR, message=Instance must be returned)
             instance = new SomeSingleton();
 
         //#OR
 
-        //Alert: Instance must be instantiated)
+        //Alert(ruleid=107, type=BUG, severity=MAJOR, message=Instance must be returned)
         instance = null;
 
         //#OR
 
-        // Alert: Instance must be returned
+        //Alert(ruleid=108, type=BUG, severity=MAJOR, message=Instance must be returned)
         return null;
-
+ 
         //#END
     }
 }
